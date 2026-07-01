927bd9f63fe4168de6eefa3603fc86248cd8a8d9
MayurKhangar <mayurkhangar25@gmail.com>
2026-07-01 16:31:24 +0530

    #onboarding controller added

---
+++ FinRiskLensAI/Controllers/OnboardingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FinRiskLensAI.Controllers
+{
+    public class OnboardingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
