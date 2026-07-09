a1e7f19e0cccb55db9be3510cf0ebf0f28d07ecc
abhinavmukwane <abhinavmukwane@gmail.com>
2026-07-09 21:10:54 +0530

    #

--- FinRiskLensAI/wwwroot/FRLScripts/Onboarding/CustOnboarding.js
+++ FinRiskLensAI/wwwroot/FRLScripts/Onboarding/CustOnboarding.js
@@ -160,7 +160,7 @@ $(function () {
 
             $('#ls1').html('<i class="bi bi-check-circle-fill"></i> Validating Enterprise Identity');
             $('#ls2').html('<i class="bi bi-check-circle-fill"></i> Retrieving Official Records');
-            $('#ls3').html('<i class="bi bi-check-circle-fill"></i> Generating Business Insights');
+            $('#ls3').html('<i class="bi bi-check-circle-fill"></i> Validating Udyam Details');
 
             $('#ls1,#ls2,#ls3').removeClass('active done');
 
